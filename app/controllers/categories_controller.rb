6a3623f8ab890119d42598429d5328a9e5688969
Rose Wan <rwan@mail.com>
2017-07-06 02:47:20 +0000

    tried categories, but error is undefined method for admin_categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,41 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @category = Category.order(created_at: :desc).all
+  end
+
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def create
+    @category = Category.new(article_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice 'This category has been created!'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to [:admin, :categories], notice 'This cateogry has been deleted'
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 
 end
