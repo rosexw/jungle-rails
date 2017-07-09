699a04df92678b9a06f9907adcd8c506425f33a4
Rose Wan <rwan@mail.com>
2017-07-09 17:13:51 +0000

    chronological order and authorize login.toreviews. fix later: redirect ReviewsController back to productID page instead of products page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,21 @@
 class ReviewsController < ApplicationController
 
+  before_filter :authorize
+
   def create
       review = Review.new(review_params)
       review.user_id = current_user.id
       review.product_id = params[:product_id]
       review.save!
-      redirect_to products_path(:id => review.product_id)
+      # redirect_to products_path(:id => review.product_id)
+      redirect_to :back
   end
 
   def delete
     id = params[:id]
     review = Review.find(id)
     review.destroy!
-    redirect_to products_path
+    redirect_to products_path(:id => review.product_id)
   end
 
   private
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ActiveRecord::Base
 
   belongs_to :category
 
+  has_many :reviews, -> { order(created_at: :desc) }
+
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
